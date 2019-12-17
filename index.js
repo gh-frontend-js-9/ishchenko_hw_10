3e62724156ae8b672581fd2c6b52cd7c1b5a8f07
alex <appofeoz00@gmail.com>
2019-12-17 23:24:49 +0200

    Small upgrade

--- index.js
+++ index.js
@@ -3,34 +3,40 @@ container.style.display = 'none';
 
 weather.onclick = () => {
     container.style.display = 'block';
-    fetch('http://api.openweathermap.org/data/2.5/weather?q=Cherkasy,ua&appid=4492edc686e7ddbf42d4433844124e8a')
+    let city = document.querySelector('.input').value;
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4492edc686e7ddbf42d4433844124e8a`)
         .then(response => { return response.json() })
         .then(data => {
             container.innerHTML = ' ';
             let weatherCity = document.createElement('p');
+            let weatherCountry = document.createElement('p');
             let weatherIcon = document.createElement('img');
             let weatherDescription = document.createElement('p');
             let flexContainer = document.createElement('div');
             let weatherTemp = document.createElement('p');
             let weatherWind = document.createElement('p');
 
-            weatherCity.setAttribute('class','city-name');
+            weatherCity.setAttribute('class','city-block');
+            weatherCountry.setAttribute('class', 'country-block');
             weatherIcon.setAttribute('src',`https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png`);
-            flexContainer.setAttribute('class', 'flex-container');
+            flexContainer.setAttribute('class', 'flex');
 
             weatherCity.innerHTML = data.name;
+            weatherCountry.innerHTML = data.sys.country;
             weatherDescription.innerHTML = data.weather[0]['description'];
             weatherTemp.innerHTML = Math.round(data.main.temp - 273) + '&deg;';
             weatherWind.innerHTML = data.wind.speed + ' m/s';
 
             container.appendChild(weatherCity);
+            container.appendChild(weatherCountry);
             container.appendChild(weatherIcon);
             container.appendChild(weatherDescription);
             container.appendChild(flexContainer);
             flexContainer.appendChild(weatherTemp);
             flexContainer.appendChild(weatherWind);
         })
-        .catch((error) => {
-            new Error(`something wrong with ${error}, click again`);
+        .catch(() => {
+            container.innerHTML = "No such city(("
         });
 }
